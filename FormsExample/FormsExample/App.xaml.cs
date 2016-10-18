2575070800c720790856cd5d040f48efff130138
Eduardo Amaral <eduarduamaral@gmail.com>
2016-10-18 18:22:34 -0200

    Capitulo 2 versão 1

---
+++ FormsExample/FormsExample/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Xamarin.Forms;
+
+namespace FormsExample
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			MainPage = new ContentPageExample();
+			//MainPage = new ContentPage
+			//{
+			//	Content = new StackLayout
+			//	{
+			//		VerticalOptions = LayoutOptions.Center,
+			//		Children = {
+			//			new Label {
+			//				HorizontalTextAlignment = TextAlignment.Center,
+			//				Text = "Welcome to Xamarin Forms"
+			//			}
+			//		}
+			//	}
+			//};
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
+
+//using Xamarin.Forms;
+
+//namespace FormsExample
+//{
+//	public partial class App : Application
+//	{
+//		public App()
+//		{
+//			InitializeComponent();
+
+//			MainPage = new FormsExamplePage();
+//		}
+
+//		protected override void OnStart()
+//		{
+//			// Handle when your app starts
+//		}
+
+//		protected override void OnSleep()
+//		{
+//			// Handle when your app sleeps
+//		}
+
+//		protected override void OnResume()
+//		{
+//			// Handle when your app resumes
+//		}
+//	}
+//}
---
+++ FormsExample/FormsExample/ContentPageExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsExample
+{
+	public class ContentPageExample : ContentPage
+	{
+		public ContentPageExample()
+		{
+			Label labelLarge = new Label
+			{
+				Text = "Label",
+				FontSize = 40,
+				HorizontalOptions = LayoutOptions.Center
+			};
+			Label labelSmall = new Label
+			{
+				Text = "This control is great for\n" + 
+					"displaying  one or more\n" + 
+					"lines of text.",
+				FontSize = 20,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					labelLarge,
+					labelSmall,
+				},
+				HeightRequest = 1500,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			this.Content = stackLayout;
+		}
+	}
+}
